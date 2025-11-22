46778b842cbfbfc9b5d73f02082e9b19c8fe9af8
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2025-11-22 10:19:53 -0400

    Actualizar buscador-infoanime.js

--- plugins/buscador-infoanime.js
+++ plugins/buscador-infoanime.js
@@ -2,27 +2,27 @@ import fetch from 'node-fetch'
 
 var handler = async (m, { conn, usedPrefix, command, text }) => {
 
-if (!text) return conn.reply(m.chat, `${emoji} Ingrese el nombre de algun anime\n\n> Ejemplo, ${usedPrefix + command} Roshidere`, m, global.rcanal)
+if (!text) return conn.reply(m.chat, `${emoji} Ingrese el nombre de algún anime\n\n> Ejemplo, ${usedPrefix + command} Blue Lock`, m, global.rcanal)
 let res = await fetch('https://api.jikan.moe/v4/manga?q=' + text)
-if (!res.ok) return conn.reply(m.chat, `${msm} Ocurrió un fallo.`, m, global.rcanal)
+if (!res.ok) return conn.reply(m.chat, `${msm} Ocurrió un fallo en la jugada.`, m, global.rcanal)
 
 let json = await res.json()
 let { chapters, title_japanese, url, type, score, members, background, status, volumes, synopsis, favorites } = json.data[0]
 let author = json.data[0].authors[0].name
-let animeingfo = `💙 Título: ${title_japanese}
-🎞️ Capítulo: ${chapters}
-💫 Transmisión: ${type}
+let animeingfo = `⚽ Título: ${title_japanese}
+🎞️ Capítulos: ${chapters}
+💫 Tipo: ${type}
 🗂 Estado: ${status}
-🗃 Volumes: ${volumes}
-🌟 Favorito: ${favorites}
+🗃 Volúmenes: ${volumes}
+🌟 Favoritos: ${favorites}
 🧮 Puntaje: ${score}
 👥 Miembros: ${members}
-🔗 Url: ${url}
+🔗 Enlace: ${url}
 👨‍🔬 Autor: ${author}
-📝 Fondo: ${background}
+📝 Contexto: ${background}
 💬 Sinopsis: ${synopsis}
  ` 
-conn.sendFile(m.chat, json.data[0].images.jpg.image_url, 'anjime.jpg', '💙 *I N F O - A N I M E* 💙\n\n' + animeingfo, fkontak, m)
+conn.sendFile(m.chat, json.data[0].images.jpg.image_url, 'anime.jpg', '⚽ *I N F O - A N I M E* ⚽\n\n' + animeingfo, fkontak, m)
 
 } 
 handler.help = ['infoanime'] 
@@ -32,4 +32,3 @@ handler.register = true
 handler.command = ['infoanime','animeinfo'] 
 
 export default handler
-
