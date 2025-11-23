94cdab05eb7b5cbbe3cac8d407405b24851e97b8
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2025-11-23 13:19:58 -0400

    Actualizar sticker-pfp.js

--- plugins/sticker-pfp.js
+++ plugins/sticker-pfp.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
     let name = conn.getName(who);
     let pp = await conn.profilePictureUrl(who, 'image').catch(() => 'https://qu.ax/cAUXY.webp');
-    await conn.sendFile(m.chat, pp, 'profile.jpg', `*Foto de perfil virtual de ${name}* 💙`, m);
+    await conn.sendFile(m.chat, pp, 'profile.jpg', `*Foto de perfil de ${name}* ⚽️`, m);
 }
 
 handler.help = ['pfp @user'];
