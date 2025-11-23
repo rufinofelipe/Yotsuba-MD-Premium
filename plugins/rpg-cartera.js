3e099d7d6e46402827390a6670fb689a34600a22
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2025-11-23 11:00:55 -0400

    Actualizar rpg-cartera.js

--- plugins/rpg-cartera.js
+++ plugins/rpg-cartera.js
@@ -1,9 +1,9 @@
 let handler = async (m, {conn, usedPrefix}) => {
 let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
 if (who == conn.user.jid) return error 
-if (!(who in global.db.data.users)) return conn.reply(m.chat, `💙 ¡Ara ara! Este usuario no está registrado en mi base de datos virtual. ✨`, m , global.rcanal)
+if (!(who in global.db.data.users)) return conn.reply(m.chat, `⚽️ ¡Error en la jugada! Este usuario no está registrado en el campo. ✨`, m , global.rcanal)
 let user = global.db.data.users[who]
-await m.reply(`${who == m.sender ? `💙 Tienes *${user.coin} ${moneda} 🎤* en tu cartera virtual de Miku! ✨` : `💙 El usuario @${who.split('@')[0]} tiene *${user.coin} ${moneda} 🎤* en su cartera virtual! ✨`}. `, null, { mentions: [who] })}
+await m.reply(`${who == m.sender ? `⚽️ Tienes *${user.coin} ${moneda} 👁* en tu cartera! ✨` : `⚽️ El usuario @${who.split('@')[0]} tiene *${user.coin} ${moneda} 👁* en su cartera! ✨`}. `, null, { mentions: [who] })}
 
 handler.help = ['wallet']
 handler.tags = ['economy']
