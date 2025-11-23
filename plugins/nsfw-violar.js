89a3427f1fe2c6a429a94158a40b139be38a7bcb
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2025-11-23 09:15:15 -0400

    Actualizar nsfw-violar.js

--- plugins/nsfw-violar.js
+++ plugins/nsfw-violar.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 let handler = async (m, { conn, usedPrefix }) => {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) {
-    return m.reply(`💙 El contenido *NSFW* está desactivado en este grupo.\n> Un administrador puede activarlo con el comando » *#nsfw on*`);
+    return m.reply(`⚽️ El contenido *NSFW* está desactivado en este grupo.\n> Un administrador puede activarlo con el comando » *#nsfw on*`);
     }
 
     let who;
