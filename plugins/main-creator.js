24bc329d4ee2fe1083ea6e3cf10afa4fdad1e410
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2025-11-27 10:02:37 -0500

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn }) => {
     const vcard = `BEGIN:VCARD
 VERSION:3.0
-FN:DuarteXV 
-ORG:DuarteXV 
+FN:DuarteXV
+ORG:DuarteXV
 TITLE:Epictetus, Enchiridion — Chapter 1 (verse 1)
-EMAIL;type=INTERNET:duartexv.ofc@gmail.com;
+EMAIL;type=INTERNET:duartexv.ofc@gmail.com
 TEL;type=CELL;waid=573244642273:+573244642273
 ADR;type=WORK:;;2-chōme-7-5 Fuchūchō;Izumi;Osaka;594-0071;Japan
 URL;type=WORK:https://www.instagram.com/duarte.mc?igsh=MW5nb2I0YjM2Ym81Mg==
-X-WA-BIZ-NAME:Isagi Yoichi Bot 
+X-WA-BIZ-NAME:Isagi Yoichi Bot
 X-WA-BIZ-DESCRIPTION:Isagi Yoichi Bot el mejor bot de WhatsApp ♡
 X-WA-BIZ-HOURS:Mo-Su 00:00-23:59
 END:VCARD`;
@@ -17,32 +17,33 @@ END:VCARD`;
         key: {
             fromMe: false,
             participant: "0@s.whatsapp.net",
-            remoteJid: "status@broadcast",
+            remoteJid: "status@broadcast"
         },
         message: {
             contactMessage: {
                 displayName: "Duartexv",
-                vcard,
-            },
-        },
+                vcard
+            }
+        }
     };
 
     await conn.sendMessage(
         m.chat,
         {
-            contacts: {
-                displayName: "Duartexv",
-                contacts: [{ vcard }],
-            },
+            contacts: [
+                {
+                    displayName: "Duartexv",
+                    vcard
+                }
+            ],
             contextInfo: {
                 externalAdReply: {
                     title: "© 2024–2025 Isagi Project",
                     body: "Contacta con el CEO del bot.",
-                    thumbnailUrl: "https://files.catbox.moe/2xlrwj.jpg",
                     mediaType: 1,
-                    renderLargerThumbnail: true,
-                },
-            },
+                    renderLargerThumbnail: true
+                }
+            }
         },
         { quoted: q }
     );
@@ -50,6 +51,6 @@ END:VCARD`;
 
 handler.help = ["owner"];
 handler.tags = ["info"];
-handler.command = ['owner', 'creador']
+handler.command = ["owner", "creador"];
 
 export default handler;
