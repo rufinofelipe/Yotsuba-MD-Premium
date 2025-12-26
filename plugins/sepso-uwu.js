5af18d8ca50edb01edfdf5904f177cfea291f991
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2025-12-26 14:53:00 -0500

    Actualizar sepso-uwu.js

--- plugins/sepso-uwu.js
+++ plugins/sepso-uwu.js
@@ -19,5 +19,6 @@ let audioMsg = {
   'buenas noches': 'https://i.top4top.io/m_2826o8rfj1.mp3',
   'sad': 'https://h.top4top.io/m_2826mcim21.mp3',
   '@5491168352204|@59172945992': 'https://l.top4top.io/m_2492i4mdu1.mp3',
+'me vale':
 'https://files.catbox.moe/3xodgw'
 }
