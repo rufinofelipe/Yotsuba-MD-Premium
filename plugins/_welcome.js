003c4f33a35335b3e8c6516a704047b82a0fdc70
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2025-12-30 10:32:02 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -34,7 +34,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
         }
 
         if (!ppUrl) {
-          ppUrl = 'https://files.catbox.moe/ax63g9.jpg'
+          ppUrl = 'https://raw.githubusercontent.com/ANDERSONARRUE/Img.2/main/upload_1767108668211.jpg'
         }
 
         console.log('📤 Enviando welcome con imagen no descargable y reenvío desde canal...')
